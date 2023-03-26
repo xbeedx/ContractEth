3f9aec6996631474af2884cedaef95cf69929e82
Buja9 <105441709+smiyou@users.noreply.github.com>
2023-03-26 21:07:04 +0200

    Lesson 4

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -20,6 +20,11 @@ contract ZombieFeeding is ZombieFactory{
 
     KittyInterface kittyContract;
 
+    modifier ownerOf(uint _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        _;
+    }
+
     function setKittyContractAddress(address _address) external onlyOwner{
         kittyContract = KittyInterface(_address);
     }
@@ -32,8 +37,7 @@ contract ZombieFeeding is ZombieFactory{
         return (_zombie.readyTime <= now);
     }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
-        require(msg.sender==zombieToOwner[_zombieId]);
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
